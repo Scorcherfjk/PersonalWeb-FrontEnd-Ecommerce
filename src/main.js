1eaa838e0c688652bc2d6626b0d756a60ac9b9d4
Fjavier De Freitas <franjavikale@gmail.com>
2020-07-26 17:47:48 -0500

    prettier run

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
-import Buefy from "buefy";
-import "buefy/dist/buefy.css";
+import Vue from 'vue';
+import App from './App.vue';
+import './registerServiceWorker';
+import router from './router';
+import store from './store';
+import Buefy from 'buefy';
+import 'buefy/dist/buefy.css';
 
 Vue.use(Buefy);
 
@@ -14,4 +14,4 @@ new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
