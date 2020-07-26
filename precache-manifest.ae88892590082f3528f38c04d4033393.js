884142d0471eff6f508c4b9f7f32018a95525c15
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-26 22:31:24 +0000

    Deploy Scorcherfjk/PersonalWeb-FrontEnd-Ecommerce to github.com/Scorcherfjk/PersonalWeb-FrontEnd-Ecommerce.git:gh-pages

---
+++ precache-manifest.ae88892590082f3528f38c04d4033393.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f94a08e0733da3551b16",
+    "url": "/PersonalWeb-FrontEnd-Ecommerce/css/app.137d5d67.css"
+  },
+  {
+    "revision": "5a627c8dddcc7dd5e949",
+    "url": "/PersonalWeb-FrontEnd-Ecommerce/css/chunk-vendors.33eb119a.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/PersonalWeb-FrontEnd-Ecommerce/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4e3f9e2c130cb1fb4e6a700ae3e19c8c",
+    "url": "/PersonalWeb-FrontEnd-Ecommerce/index.html"
+  },
+  {
+    "revision": "76d8dd1c2b66c340126c",
+    "url": "/PersonalWeb-FrontEnd-Ecommerce/js/about.9990037d.js"
+  },
+  {
+    "revision": "f94a08e0733da3551b16",
+    "url": "/PersonalWeb-FrontEnd-Ecommerce/js/app.bdeff1ae.js"
+  },
+  {
+    "revision": "5a627c8dddcc7dd5e949",
+    "url": "/PersonalWeb-FrontEnd-Ecommerce/js/chunk-vendors.ba45ee91.js"
+  },
+  {
+    "revision": "eebaff20c46e1413c5604c4a1b63024d",
+    "url": "/PersonalWeb-FrontEnd-Ecommerce/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/PersonalWeb-FrontEnd-Ecommerce/robots.txt"
+  }
+]);
